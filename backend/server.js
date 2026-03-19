557e3430de1b40f8c266a50091c4d7f8a73fae1d
Asha Ahmed <ashaahmed012345@gmail.com>
2026-03-19 06:25:24 -0500

    Fix CORS and trust proxy

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ const propertyRoutes = require('./routes/propertyRoutes');
 
 // Create Express app
 const app = express();
+app.set('trust proxy', 1);
 
 // Connect to Database
 connectDatabase();
@@ -25,7 +26,7 @@ app.use(helmet());
 app.use(cors({
   origin: ['https://rentnesto.xyz',
            'https://www.rentnesto.xyz',
-           'https://rentnesto-nextjs-vpid.vercel.app/', 
+           'https://rentnesto-nextjs-vpid.vercel.app', 
            'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
