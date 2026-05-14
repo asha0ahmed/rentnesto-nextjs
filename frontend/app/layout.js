0c7d6540b4eb9a1b4f00ed80e1249158b25bd179
Asha Ahmed <ashaahmed012345@gmail.com>
2026-05-14 15:47:20 -0600

    SEO: add Google Analytics GA4

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -27,6 +27,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-26NMK5BYWG"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-26NMK5BYWG');
+          `
+        }} />
+      </head>
       <body>
         <Providers>
           <Navbar />
